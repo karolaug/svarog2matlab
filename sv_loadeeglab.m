00095de0604424669c0ad0496a81f6b46d6846ce
Karol Augustin <karol@augustin.pl>
2012-03-12 14:10:18 +0100

    Functions organisation

--- sv_loadeeglab.m
+++ sv_loadeeglab.m
@@ -6,13 +6,6 @@
 %License can be found in license
 
 %Loads eeglab with data from sv_data matrix and sv_info variable, supports channel location. Takes filename without extension as argument.
-function sv_loadeeglab(file_name);
+function sv_loadeeglab(filename);
     [info, data] = sv_loaddata(file_name);
-    global ALLEEG EEG CURRENTSET ALLCOM
-    [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
-    global EEG
-    EEG = pop_importdata('data', data, 'dataformat', 'array', 'srate', info.fs);
-    chanlocs = struct('labels', info.channames);
-    EEG.chanlocs = pop_chanedit(chanlocs);
-    [ALLEEG EEG CURRENTSET ] = eeg_store(ALLEEG, EEG);
-    eeglab redraw;
+    sv_loadeeg(info, data)
